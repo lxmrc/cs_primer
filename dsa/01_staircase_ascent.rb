787d4edcef5aca0a430036fca91e59804f6e77db
Alex Mercea <alex@lxmrc.com>
2024-06-12 18:45:07 +1000

    Update staircase ascent

--- dsa/01_staircase_ascent.rb
+++ dsa/01_staircase_ascent.rb
@@ -1,7 +1,7 @@
 def staircase_ascent(n)
+  return 1 if n == 0
   return 1 if n == 1
   return 2 if n == 2
-  return 4 if n == 3
 
   staircase_ascent(n - 1) + staircase_ascent(n - 2) + staircase_ascent(n - 3)
 end
