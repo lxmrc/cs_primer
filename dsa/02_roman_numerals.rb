220d5ce948c828341f4197fa9b0088c59db0329f
Alex Mercea <alex@lxmrc.com>
2024-06-12 19:15:32 +1000

    Complete convert to Roman

---
+++ dsa/02_roman_numerals.rb
@@ -0,0 +1,75 @@
+require "pry-byebug"
+
+module RomanNumerals
+  def self.to_roman(n)
+    roman = ""
+    remaining = n
+
+    if remaining >= 1000
+      roman += "M" * (remaining / 1000)
+      remaining -= (remaining / 1000) * 1000
+    end
+
+    if remaining >= 900
+      roman += "CM"
+      remaining -= 900
+    end
+
+    if remaining >= 500
+      roman += "D"
+      remaining -= 500
+    end
+
+    if remaining >= 400
+      roman += "CD"
+      remaining -= 400
+    end
+
+    if remaining >= 100
+      roman += "C" * (remaining / 100)
+      remaining -= (remaining / 100) * 100
+    end
+
+    if remaining >= 90
+      roman += "XC"
+      remaining -= 90
+    end
+
+    if remaining >= 50
+      roman += "L"
+      remaining -= 50
+    end
+
+    if remaining >= 40
+      roman += "XL"
+      remaining -= 40
+    end
+    
+    if remaining >= 10
+      roman += "X" * (remaining / 10)
+      remaining -= (remaining / 10) * 10
+    end
+
+    if remaining == 9
+      roman += "IX"
+      remaining -= 9
+    end
+
+    if remaining >= 5
+      roman += "V"
+      remaining -= 5
+    end
+
+    if remaining == 4
+      roman += "IV"
+      remaining -= 4
+    end
+
+    if remaining > 0
+      roman += "I" * remaining
+      remaining -= n
+    end
+
+    roman
+  end
+end
---
+++ dsa/02_roman_numerals_test.rb
@@ -0,0 +1,81 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '02_roman_numerals'
+
+class RomanNumeralsTest < Minitest::Test
+  def test_1
+    assert_equal 'I', RomanNumerals.to_roman(1)
+  end
+
+  def test_2
+    assert_equal 'II', RomanNumerals.to_roman(2)
+  end
+
+  def test_3
+    assert_equal 'III', RomanNumerals.to_roman(3)
+  end
+
+  def test_4
+    assert_equal 'IV', RomanNumerals.to_roman(4)
+  end
+
+  def test_5
+    assert_equal 'V', RomanNumerals.to_roman(5)
+  end
+
+  def test_6
+    assert_equal 'VI', RomanNumerals.to_roman(6)
+  end
+
+  def test_9
+    assert_equal 'IX', RomanNumerals.to_roman(9)
+  end
+
+  def test_27
+    assert_equal 'XXVII', RomanNumerals.to_roman(27)
+  end
+
+  def test_48
+    assert_equal 'XLVIII', RomanNumerals.to_roman(48)
+  end
+
+  def test_59
+    assert_equal 'LIX', RomanNumerals.to_roman(59)
+  end
+
+  def test_93
+    assert_equal 'XCIII', RomanNumerals.to_roman(93)
+  end
+
+  def test_141
+    assert_equal 'CXLI', RomanNumerals.to_roman(141)
+  end
+
+  def test_163
+    assert_equal 'CLXIII', RomanNumerals.to_roman(163)
+  end
+
+  def test_402
+    assert_equal 'CDII', RomanNumerals.to_roman(402)
+  end
+
+  def test_575
+    assert_equal 'DLXXV', RomanNumerals.to_roman(575)
+  end
+
+  def test_911
+    assert_equal 'CMXI', RomanNumerals.to_roman(911)
+  end
+
+  def test_1024
+    assert_equal 'MXXIV', RomanNumerals.to_roman(1024)
+  end
+
+  def test_3000
+    assert_equal 'MMM', RomanNumerals.to_roman(3000)
+  end
+
+  def test_3999
+    assert_equal 'MMMCMXCIX', RomanNumerals.to_roman(3999)
+  end
+end
